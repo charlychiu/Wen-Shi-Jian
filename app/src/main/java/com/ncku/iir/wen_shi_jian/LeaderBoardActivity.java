0de7427877061c6680fe5432ca8da944381063b8
charlychiu <charlychiu@gmail.com>
2019-06-17 16:50:09 +0800

    Add best & current ranking

--- app/src/main/java/com/ncku/iir/wen_shi_jian/LeaderBoardActivity.java
+++ app/src/main/java/com/ncku/iir/wen_shi_jian/LeaderBoardActivity.java
@@ -1,6 +1,7 @@
 package com.ncku.iir.wen_shi_jian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.ncku.iir.wen_shi_jian.core.Global;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,6 +32,8 @@ public class LeaderBoardActivity extends AppCompatActivity {
     TextView firstView;
     TextView secondView;
     TextView thirdView;
+    TextView bestView;
+    TextView currentView;
 
     Button againButton;
     Button nextButton;
@@ -42,10 +46,14 @@ public class LeaderBoardActivity extends AppCompatActivity {
         firstView = (TextView)findViewById(R.id.firstView);
         secondView = (TextView)findViewById(R.id.secondView);
         thirdView = (TextView)findViewById(R.id.thirdView);
+        bestView = (TextView)findViewById(R.id.bestView);
+        currentView = (TextView)findViewById(R.id.currentView);
 
         firstView.setText("第一名: Loading...");
         secondView.setText("第二名: Loading...");
         thirdView.setText("第三名: Loading...");
+        bestView.setText("最好排名 */*");
+        currentView.setText("此次排名 */*");
 
         againButton = findViewById(R.id.againButton);
         againButton.setOnClickListener(new Button.OnClickListener() {
@@ -89,6 +97,42 @@ public class LeaderBoardActivity extends AppCompatActivity {
                 Log.d("LeaderBoardActivity", getMapUserAndScore(hm1, 1));
                 Log.d("LeaderBoardActivity", getMapUserAndScore(hm1, 2));
 
+                // Count for ranking
+                int totalRankCount = scores.size();
+                int userCurrentScore = Global.score;
+                int currentRankCount = 0;
+                int userCurrentRank = -1;
+                LinkedHashMap<String, Integer> newMap = new LinkedHashMap<>(hm1);
+
+                for (Map.Entry<String, Integer> entry : newMap.entrySet()) {
+                    String key = entry.getKey();
+                    int value = entry.getValue();
+                    Log.d("LeaderBoardActivity", "Order from map: " + key + " " + value);
+                    currentRankCount += 1;
+                    if (userCurrentScore == value) {
+                        userCurrentRank = currentRankCount;
+                        break;
+                    }
+                }
+                String currentString = String.format("此次排名 %d/%d", userCurrentRank ,totalRankCount);
+                currentView.setText(currentString);
+
+                SharedPreferences mPreferences = com.ncku.iir.wen_shi_jian.Global.getPrefInstance();
+                String tmpUserName = mPreferences.getString("username", "Not Value");
+                currentRankCount = 0;
+                userCurrentRank = -1;
+                for (Map.Entry<String, Integer> entry : newMap.entrySet()) {
+                    String key = entry.getKey();
+                    int value = entry.getValue();
+                    Log.d("LeaderBoardActivity", "Order from map: " + key + " " + value);
+                    currentRankCount += 1;
+                    if (tmpUserName.equals(key)) {
+                        userCurrentRank = currentRankCount;
+                        break;
+                    }
+                }
+                String bestString = String.format("最好排名 %d/%d", userCurrentRank ,totalRankCount);
+                bestView.setText(bestString);
             }
 
             @Override
