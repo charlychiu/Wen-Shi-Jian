bb789be9b6c167e37aaf2940da922ccd72fe8ce0
charlychiu <charlychiu@gmail.com>
2019-06-17 16:20:47 +0800

    Add score upload

--- app/src/main/java/com/ncku/iir/wen_shi_jian/FeedbackActivity.java
+++ app/src/main/java/com/ncku/iir/wen_shi_jian/FeedbackActivity.java
@@ -1,6 +1,7 @@
 package com.ncku.iir.wen_shi_jian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ncku.iir.wen_shi_jian.core.Global;
 
 public class FeedbackActivity extends AppCompatActivity {
@@ -26,6 +29,13 @@ public class FeedbackActivity extends AppCompatActivity {
 
         scoreView = findViewById(R.id.scoreView);
         scoreView.setText("分數 : "+String.valueOf(Global.score));
+
+        // TODO: Step 7 save score to DB
+        SharedPreferences mPreferences = com.ncku.iir.wen_shi_jian.Global.getPrefInstance();
+        String tmpUserName = mPreferences.getString("username", "Not Value");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("score");
+        myRef.child(tmpUserName).setValue(Global.score);
 
         againButton = findViewById(R.id.againButton);
         againButton.setOnClickListener(new Button.OnClickListener() {
